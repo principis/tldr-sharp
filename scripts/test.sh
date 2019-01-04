4027bc3c387539d770db4c59cdf6f232bf50f96f
Arthur Bols <arthur.bolsbe@gmail.com>
2019-01-04 22:34:04 +0100

    fix unable to find an interpreter in test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,104 +5,104 @@ cd tldr-sharp/bin/Release
 chmod +x tldr_sharp.exe
 
 # Test update
-./tldr_sharp.exe -u >/dev/null
+mono tldr_sharp.exe -u >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
-./tldr_sharp.exe --update >/dev/null
+mono tldr_sharp.exe --update >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test clear cache
-./tldr_sharp.exe -c >/dev/null
+mono tldr_sharp.exe -c >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
-./tldr_sharp.exe --clear-cache >/dev/null
+mono tldr_sharp.exe --clear-cache >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test list
-./tldr_sharp.exe --list >/dev/null
+mono tldr_sharp.exe --list >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test list-all
-./tldr_sharp.exe -a >/dev/null
+mono tldr_sharp.exe -a >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
-./tldr_sharp.exe --list-all >/dev/null
+mono tldr_sharp.exe --list-all >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
-./tldr_sharp.exe tar >/dev/null
+mono tldr_sharp.exe tar >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test help
-./tldr_sharp.exe -h >/dev/null
+mono tldr_sharp.exe -h >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
-./tldr_sharp.exe --help >/dev/null
+mono tldr_sharp.exe --help >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test non-existing command
-./tldr_sharp.exe -x >/dev/null
+mono tldr_sharp.exe -x >/dev/null
 if [ $? == 0 ]; then
     exit 1
 fi
-./tldr_sharp.exe --fake >/dev/null
+mono tldr_sharp.exe --fake >/dev/null
 if [ $? == 0 ]; then
     exit 1
 fi
  
 # Test page
-./tldr_sharp.exe tar >/dev/null
+mono tldr_sharp.exe tar >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
  
 # Test non-existing page
-./tldr_sharp.exe giberishdsfsd >/dev/null
+mono tldr_sharp.exe giberishdsfsd >/dev/null
 if [ $? == 0 ]; then
     exit 1
 fi 
 
 # test os 
-./tldr_sharp.exe tldr --os=linux >/dev/null
+mono tldr_sharp.exe tldr --os=linux >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
 
 # Test os non-existing page
-./tldr_sharp.exe dir --os=linux >/dev/null
+mono tldr_sharp.exe dir --os=linux >/dev/null
 if [ $? == 0 ]; then
     exit 1
 fi
 
 # Test list-languages
-./tldr_sharp.exe tldr --list-languages >/dev/null
+mono tldr_sharp.exe tldr --list-languages >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
 
 # Test list-os
-./tldr_sharp.exe tldr --list-os >/dev/null
+mono tldr_sharp.exe tldr --list-os >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
 
 # Test version
-./tldr_sharp.exe tldr -V >/dev/null
+mono tldr_sharp.exe tldr -V >/dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
