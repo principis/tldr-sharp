ace9974078b6e12d151985c251b3032a528b2a4f
Arthur Bols <arthur@bols.dev>
2020-01-24 10:29:42 +0100

    Fixed and improved zsh completions

--- scripts/completions.zsh
+++ scripts/completions.zsh
@@ -1,23 +1,23 @@
-#compdef tldr
+compdef _tldr tldr
   
-local -a pages oses languages
-pages=$(tldr -a)
-platforms='(linux osx sunos windows)'
-languages=$(tldr --list-languages)
+function _tldr() {
+local -a platforms
+platforms=(linux osx sunos windows)
 
 _arguments \
-  '(- *)'{-a,--list-all}'[List all pages]' \
-  '(- *)'{-c,--clear-cache}'[Clear the cache]' \
-  '(- *)'{-f,--render}'[Render a specific markdown file]' \
-  '(- *)'{-h,--help}'[Display this help text]' \
-  '(- *)'{-l,--list}'[List all pages for the current platform]' \
-  "--list-os[List all platforms]" \
+  {-a,--list-all}'[List all pages]' \
+  {-c,--clear-cache}'[Clear the cache]' \
+  {-f,--render}'[Render a specific markdown file]' \
+  {-h,--help}'[Display this help text]' \
+  {-l,--list}'[List all pages for the current platform and language]' \
+  '--list-os[List all platforms]' \
   '--list-languages[List all languages]' \
-  '(- *)'{-L,--language}'[Specifies the preferred language]:lang:(${languages})' \
-  '(- *)'{-m,--markdown}'[Show the markdown source of a page]' \
-  '(- *)'{-p,--platform}'[Override the default OS]:os:(${oses})' \
-  '(- *)'{-s,--search}'[Search for a string]' \
-  '(- *)'{-u,--update}'[Update the cache]' \
+  {-L,--language}'[Specifies the preferred language]:lang:(("${(@f)$(tldr --list-languages)}"))' \
+  {-m,--markdown}'[Show the markdown source of a page]' \
+  {-p,--platform}'[Override the default platform]:platform:(${platforms})' \
+  {-s,--search}'[Search for a string]' \
+  {-u,--update}'[Update the cache]' \
   '--self-update[Check for tldr-sharp updates]' \
-  '(- *)'{-v,--version}'[Show version information]' \
-  "*:page:(${pages})" && return 0
+  {-v,--version}'[Show version information]' \
+  ":page:($(tldr -a))" && return 0
+}
