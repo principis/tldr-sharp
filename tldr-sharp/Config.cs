d0f495093cf0f6f7667fce1ca8fc665dffc1c446
Arthur Bols <arthur@bols.dev>
2023-11-17 20:42:58 +0100

    Fix alternative remove url

--- tldr-sharp/Config.cs
+++ tldr-sharp/Config.cs
@@ -30,7 +30,7 @@ namespace tldr_sharp
         internal const string RemoteUrl = "https://raw.githubusercontent.com/tldr-pages/tldr/main/pages";
 
         internal const string ArchiveRemote = "https://tldr.sh/assets/tldr.zip";
-        internal const string ArchiveAlternativeRemote = "https://github.com/tldr-pages/tldr-pages.github.io/raw/main/assets/tldr.zip";
+        internal const string ArchiveAlternativeRemote = "https://raw.githubusercontent.com/tldr-pages/tldr-pages.github.io/main/assets/tldr.zip";
 
         internal const string NewPageUrl = "https://github.com/tldr-pages/tldr/issues/new?title=page%20request:%20";
         internal const string NewTranslationUrl = "https://github.com/tldr-pages/tldr/blob/main/CONTRIBUTING.md#translations";
