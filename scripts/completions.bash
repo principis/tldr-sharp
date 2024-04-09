fc3bf6d05aac01fff80dc0034d327c4e12b9cbac
Arthur Bols <arthur@bols.dev>
2024-04-09 19:51:22 +0200

    Improve Bash completions

--- scripts/completions.bash
+++ scripts/completions.bash
@@ -3,17 +3,35 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 
-_tldr_sharp() 
-{
-        pages=$(tldr -a)
-        commands='-h --help -a --list-all -c --clear-cache -f --render -l --list --list-os --list-languages -L --language -m --markdown -p --platform -s --search -u --update --self-update -v --version \
-                --lang= --list-languages'
-        COMPREPLY=()
+_tldr-sharp_completions_filter() {
+    local words="$1"
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local result=()
 
-        if [ $COMP_CWORD = 1 ]; then
-                COMPREPLY=( $(compgen -W "$pages" -- $2) )
-                COMPREPLY+=( $(compgen -W "$commands" -- $2) )
-        fi
+    if [[ "${cur:0:1}" == "-" ]]; then
+        echo "$words"
+
+    else
+        for word in $words; do
+            [[ "${word:0:1}" != "-" ]] && result+=("$word")
+        done
+
+        echo "${result[*]}"
+
+    fi
 }
 
-complete -F _tldr_sharp tldr-sharp
+_tldr-sharp_completions() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+
+    case "${cur[0]}" in
+    '-'*)
+        while read -r; do COMPREPLY+=("$REPLY"); done < <(compgen -W "$(_tldr-sharp_completions_filter "-a --list-all -c --clear-cache -f --render -h --help -l --list --list-platforms --list-languages -L --language -m --markdown -p --platform -s --search -u --update --self-update -v --version")" -- "$cur")
+        ;;
+
+    *)
+        while read -r; do COMPREPLY+=("$REPLY"); done < <(compgen -W "$(_tldr-sharp_completions_filter "$(tldr-sharp -a)")" -- "$cur")
+        ;;
+    esac
+} &&
+    complete -F _tldr-sharp_completions tldr-sharp
