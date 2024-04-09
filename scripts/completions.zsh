7f38c30e27d805e1a25332145dc77640c7bcc32d
Arthur Bols <arthur@bols.dev>
2024-04-09 17:52:10 +0200

    Improve Zsh completions

--- scripts/completions.zsh
+++ scripts/completions.zsh
@@ -3,26 +3,42 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 
-_tldr_sharp() {
-  local -a platforms
-  platforms=(linux osx sunos windows)
+(( $+functions[_tldr_sharp_list_platforms] )) ||
+  _tldr_sharp_list_platforms() {
+    local -a _platforms
+    _platforms=($(tldr-sharp --list-platforms))
+    _describe 'platform' _platforms
+  }
 
+(( $+functions[_tldr_sharp_list_languages] )) ||
+  _tldr_sharp_list_languages() {
+    local -a _langs
+    _langs=("${(f)$(tldr-sharp --list-languages)//$'\t'/}")
+    _describe 'language' _langs
+  }
+
+(( $+functions[_tldr_sharp_pages] )) ||
+  _tldr_sharp_pages() {
+    compadd - ${(f)"$(tldr-sharp -a)"}
+  }
+
+_tldr-sharp() {
   _arguments \
     {-a,--list-all}'[List all pages]' \
     {-c,--clear-cache}'[Clear the cache]' \
-    {-f,--render}'[Render a specific markdown file]' \
+    {-f,--render}'[Render a specific markdown file]: : _files' \
     {-h,--help}'[Display this help text]' \
     {-l,--list}'[List all pages for the current platform and language]' \
-    '--list-os[List all platforms]' \
+    '--list-platforms[List all platforms]' \
     '--list-languages[List all languages]' \
-    {-L,--language}'[Specifies the preferred language]:lang:(("${(@f)$(tldr --list-languages)}"))' \
+    {-L,--language}'[Specifies the preferred language]:language:_tldr_sharp_list_languages' \
     {-m,--markdown}'[Show the markdown source of a page]' \
-    {-p,--platform}'[Override the default platform]:platform:(${platforms})' \
+    {-p,--platform}'[Override the default platform]:platform:_tldr_sharp_list_platforms' \
     {-s,--search}'[Search for a string]' \
     {-u,--update}'[Update the cache]' \
     '--self-update[Check for tldr-sharp updates]' \
     {-v,--version}'[Show version information]' \
-    ':page:("${(@f)$(tldr-sharp -a)}")' && return 0
+    '1::page:_tldr_sharp_pages' && return 0
 }
 
-_tldr_sharp
+_tldr-sharp
